5711322f2f48b7b237643e89fc0ef98d56247cb7
Philipp Konopac <philipp.konopac@gmail.com>
2018-11-10 17:46:08 +0100

    Code input window added

--- Content/Scripts/Interpreter.py
+++ Content/Scripts/Interpreter.py
@@ -4,21 +4,41 @@ class Interpreter:
 
 	def interpret(self, code):
 		def rollNorth():
-			ue.log('north')
+			ue.log('rollNorth')
 			self.uobject.AddToQueue('rollNorth')
 			
 		def rollSouth():
-			ue.log('south')
+			ue.log('rollSouth')
 			self.uobject.AddToQueue('rollSouth')
 			
 		def rollWest():
-			ue.log('west')
+			ue.log('rollWest')
 			self.uobject.AddToQueue('rollWest')
 			
 		def rollEast():
-			ue.log('east')
+			ue.log('rollEast')
 			self.uobject.AddToQueue('rollEast')
 		
+		def checkNorth():
+			ue.log('checkNorth')
+			self.uobject.AddToQueue('checkNorth')
+			return True
+		
+		def checkSouth():
+			ue.log('checkSouth')
+			self.uobject.AddToQueue('checkSouth')
+			return True
+			
+		def checkWest():
+			ue.log('checkWest')
+			self.uobject.AddToQueue('checkWest')
+			return True
+			
+		def checkEast():
+			ue.log('checkEast')
+			self.uobject.AddToQueue('checkEast')
+			return True
+
 		ue.log('----------')
 		exec code in globals(), locals()
 		self.uobject.PlayQueue()
