8bb27a075eea8b591aa76e4b7c39c37350361f09
Philipp Konopac <philipp.konopac@gmail.com>
2018-12-18 21:11:50 +0100

    new levels + changed ingame language to german

---
+++ solutions/level5.py
@@ -0,0 +1,12 @@
+for x in range(7):
+	roll_north()
+for x in range(4):
+	roll_east()
+for x in range(7):
+	roll_south()
+for x in range(4):
+	roll_east()
+for x in range(7):
+	roll_north()
+for x in range(2):
+	roll_west()
---
+++ solutions/level6.py
@@ -0,0 +1,9 @@
+for x in range(4):
+	for y in range(2):
+		roll_north()
+	for y in range(5):
+		roll_west()
+	for y in range(2):
+		roll_north()
+	for y in range(5):
+		roll_east()
